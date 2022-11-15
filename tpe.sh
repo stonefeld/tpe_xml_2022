74857bee409933de66e1cb84a446de345e10bdf4
stonefeld <theo.stanfield@gmail.com>
2022-11-15 14:29:42 -0300

    Bash script funcionando e indentado del md arreglado

--- tpe.sh
+++ tpe.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Agrego las variables del classpath
+# Agregamos las variables del classpath
 source shell
 
-# Declarando los nombres de los archivos a utilizar
+# Declaramos los nombres de los archivos a utilizar
 info_file='season_info.xml'
 summaries_file='season_summaries.xml'
 data_file='season_data.xml'
@@ -14,9 +14,9 @@ md_file='season_page.md'
 # TODO(ts): meter los errores en un xml con error
 print_error() {
     [ "$1" ] && echo "ERROR: $1"
-    echo "Usage: tpe.sh <season_id>"
+    echo 'Usage: tpe.sh <season_id>'
     if [ "$1" ]; then
-        echo "$1" > "$data_file"
+        echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?><season_data xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-Instance\" xsi:noNamespaceSchemaLocation=\"season_data.xsd\"><error>$1</error></season_data>" > "$data_file"
         generate_md
     fi
     exit
@@ -27,28 +27,35 @@ generate_md() {
     java net.sf.saxon.Transform -s:"$data_file" -xsl:"$generate_md_file" > "$md_file" 2>/dev/null
 }
 
-# Obtengo el id de la season
+# Obtenemos el id de la season
 [ -z "$*" ] && print_error
 season_id="$1"
 
-# Verifico que el id sea valido
+# Verificamos que el id sea valido
 [ -f seasons.xml ] || print_error "File 'seasons.xml' is required"
-grep -E "\s*<season\s+id=\"$season_id\".*/>" seasons.xml 1>/dev/null
+grep -E "\s*<season\s+id=\"$season_id\".*/>" seasons.xml &>/dev/null
 [ "$?" -ne 0 ] && print_error "The given 'season_id' is not valid"
 
-# Verifico la existencia de una api key
+# Verificamos la existencia de una api key
 [ -f api_key.txt ] || { [ -z "$SPORTRADAR_API" ] && print_error "File 'api_key.txt' or envvar 'SPORTRADAR_API' are required"; }
 SPORTRADAR_API="$(cat api_key.txt)"
 
-# Declarando las urls para las correspondientes queries
-info_url="https://api.sportradar.com/soccer/trial/v4/en/seasons/${season_id}/info.xml?api_key=${SPORTRADAR_API}"
-summaries_url="https://api.sportradar.us/soccer/trial/v4/en/seasons/${season_id}/summaries.xml?api_key=${SPORTRADAR_API}"
+# Declaramos las urls para las correspondientes queries
+base_url="https://api.sportradar.com/soccer/trial/v4/en/seasons/${season_id}"
+info_url="$base_url/info.xml?api_key=${SPORTRADAR_API}"
+summaries_url="$base_url/summaries.xml?api_key=${SPORTRADAR_API}"
 
+# Realizamos ambos GET Request para obtener los archivos de info y summaries
 curl $info_url -o $info_file 2>/dev/null
 curl $summaries_url -o $summaries_file 2>/dev/null
+[ "$?" -ne 0 ] && print_error "Couldn't reach 'api.sportradar.com'. Make sure you have a stable internet connection."
 
+# Eliminamos el namespace en cada xml para que las consultas por XQuery funcionen
 sed -i 's%\sxmlns="http://schemas.sportradar.com/sportsapi/soccer/v4"%%' "$info_file"
 sed -i 's%\sxmlns="http://schemas.sportradar.com/sportsapi/soccer/v4"%%' "$summaries_file"
 
-java net.sf.saxon.Query "$extract_data_file" > "$data_file"
+# Realizamos la consulta por XQuery que generara el archivo 'season_data.xml'
+java net.sf.saxon.Query "$extract_data_file" > "$data_file" 2>/dev/null
+java dom.Writer -v -s "$data_file" 2>&1 >/dev/null | grep -E '^\[Error\]' &>/dev/null
+[ "$?" -eq 0 ] && print_error "'season_data.xml' did not match the requirements on 'season_data.xsd'"
 generate_md
